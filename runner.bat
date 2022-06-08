b928a63003962f12c7c824f209c75bb433ea589d
Deendayal Kumawat <44318598+ddsha441981@users.noreply.github.com>
2022-06-08 08:19:00 +0530

    Add files via upload
    either double click on exe file or runner.bat file choices yours

---
+++ runner.bat
@@ -0,0 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB9QXwyDaD/rX+1N1Mz+/OGTrV0hRvcxdorJ5uDdc64a6UqE
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdFy5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdFy5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQICPRVaSRbMOXO/Zg==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbAs=
+::ZQ05rAF9IAHYFVzEqQICPRVaQgGQXA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDDaXMmW/FNU=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQITOh5VWAGGfEmjRpEZ7eb/7vOGrg02Qe8+b47Pug==
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+
+@echo Process Completed!!!!
+@echo Follow me on
+@echo https://www.github.com/ddsha441981
